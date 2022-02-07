ecaa06608a0e022748765e39ca6a71312882263e
Conner <hetti031@morris.umn.edu>
2022-02-07 17:06:05 -0600

    Attempt to fix more check style under FilterTodos.java

--- server/src/test/java/umm3601/user/FilterTodos.java
+++ server/src/test/java/umm3601/user/FilterTodos.java
@@ -11,14 +11,15 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings({ "MagicNumber" })
 public class FilterTodos {
     @Test
   public void limitTodos() throws IOException {
     TodoDatabase db = new TodoDatabase("/todos.json");
     Todo[] allTodos = db.listTodos(new HashMap<>());
 
-    Todo[] OwnedByFry = db.limitTodos(allTodos, 3);
-    assertEquals(3, OwnedByFry.length, "Incorrect number of todos with owner Fry");
+    Todo[] ownedByFry = db.limitTodos(allTodos, 3);
+    assertEquals(3, ownedByFry.length, "Incorrect number of todos with owner Fry");
 
     }
 
@@ -27,11 +28,11 @@ public class FilterTodos {
       TodoDatabase db = new TodoDatabase("/todos.json");
       Todo[] allTodos = db.listTodos(new HashMap<>());
 
-      Todo[] OwnedByFry = db.filterTodosByOwner(allTodos, "Fry");
-      assertEquals(61, OwnedByFry.length, "Incorrect number of todos with owner Fry");
+      Todo[] ownedByFry = db.filterTodosByOwner(allTodos, "Fry");
+      assertEquals(61, ownedByFry.length, "Incorrect number of todos with owner Fry");
 
-      Todo[] OwnedByWorkman = db.filterTodosByOwner(allTodos, "Workman");
-      assertEquals(49, OwnedByWorkman.length, "Incorrect number of todos with owner Workman");
+      Todo[] ownedByWorkman = db.filterTodosByOwner(allTodos, "Workman");
+      assertEquals(49, ownedByWorkman.length, "Incorrect number of todos with owner Workman");
     }
 
     @Test
@@ -51,7 +52,7 @@ public class FilterTodos {
     }
 
     @Test
-    public void filterBodyContent() throws IOException{
+    public void filterBodyContent() throws IOException {
       TodoDatabase db = new TodoDatabase("/todos.json");
       Map<String, List<String>> queryParams = new HashMap<>();
 
